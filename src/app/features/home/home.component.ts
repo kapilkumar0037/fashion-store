aa4dd447fbdf1975977c73f197bceafad9d507d9
Kapil Kumar <kapilkumar0037@gmail.com>
2025-07-08 17:50:11 +0530

    blog card created

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CarouselComponent, FeaturedCarouselComponent, TestimonialCarouselComponent } from '../../shared';
+import { BlogCardComponent } from '../../shared/components/blog-card/blog-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ FeaturedCarouselComponent, TestimonialCarouselComponent, CarouselComponent],
+  imports: [ FeaturedCarouselComponent, TestimonialCarouselComponent, CarouselComponent, BlogCardComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
