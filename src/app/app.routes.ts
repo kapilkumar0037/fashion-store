c4b49e70e31756a1c76704e2a729f3e3e472a0d3
Kapil Kumar <kapilkumar0037@gmail.com>
2025-07-09 21:41:20 +0530

    product details screen added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from './core';
-import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'auth', pathMatch: 'full' },
     { path: 'auth', component: AuthLayoutComponent, children: [
         { path: '',
              loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent) },
+             { path: 'product-details/:id',
+             loadComponent: () => import('./shared/components/product-details/product-details.component').then(m => m.ProductDetailsComponent) },
     ]},
     { path: "**", redirectTo: 'auth', pathMatch: 'full' }
 
