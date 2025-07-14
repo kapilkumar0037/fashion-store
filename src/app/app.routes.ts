3c44f5881d5495df3f18a284aac12c47bca4b62a
Kapil Kumar <kapilkumar0037@gmail.com>
2025-07-14 21:44:51 +0530

    static pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,36 @@ import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from './core';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'auth', pathMatch: 'full' },
-    { path: 'auth', component: AuthLayoutComponent, children: [
-        { path: '',
-             loadComponent: () => import('./features/home/home.component') },
-             { path: 'product-details/:id',
-             loadComponent: () => import('./features/products/components/product-details/product-details.component')},
-             { path: 'checkout',
-             loadComponent: () => import('./features/checkout/checkout.component') },
-    ]},
+    { path: '', redirectTo: '', pathMatch: 'full' },
+    {
+        path: '', component: AuthLayoutComponent, children: [
+            {
+                path: '',
+                loadComponent: () => import('./features/home/home.component')
+            },
+            {
+                path: 'product-details/:id',
+                loadComponent: () => import('./features/products/components/product-details/product-details.component')
+            },
+            {
+                path: 'checkout',
+                loadComponent: () => import('./features/checkout/checkout.component')
+            },
+            {
+                path: 'about',
+                loadComponent: () => import('./features/static-pages/about-us/about-us.component')
+            },
+            {
+                path: 'privacy-policy',
+                loadComponent: () => import('./features/static-pages/privacy/privacy.component')
+            },
+            {
+                path: 'terms-of-service',
+                loadComponent: () => import('./features/static-pages/terms-and-conditions/terms-and-conditions.component')
+            },
+        ]
+    },
+
     { path: "**", redirectTo: 'auth', pathMatch: 'full' }
 
 ];
