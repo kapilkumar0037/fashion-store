c0430f2dd9d3ac582581b0c32ffbc7601618ecf1
Kapil Kumar <kapilkumar0037@gmail.com>
2025-07-21 20:34:20 +0530

    search component added

--- src/app/core/components/nav/nav.component.ts
+++ src/app/core/components/nav/nav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SearchComponent } from '@shared/components/search/search.component';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterLink],
+  imports: [RouterLink, SearchComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
