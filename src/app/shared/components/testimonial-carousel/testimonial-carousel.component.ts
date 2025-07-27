eba5adebbf42eb15f13b20bd9c0f070734ded285
Kapil Kumar <kapilkumar0037@gmail.com>
2025-07-27 16:50:53 +0530

    testimonial

--- src/app/shared/components/testimonial-carousel/testimonial-carousel.component.ts
+++ src/app/shared/components/testimonial-carousel/testimonial-carousel.component.ts
@@ -10,38 +10,8 @@ import { ImageComponent } from "../image/image.component";
   styleUrl: './testimonial-carousel.component.scss'
 })
 export class TestimonialCarouselComponent {
- testimonials = input<IFeaturedTestimonials[]>([])
-
- itemsPerSlide = 1;
-  slides = [
-    { img: "/assets/img/gallery/latest1.jpg.webp" },
-    { img: "/assets/img/gallery/latest2.jpg.webp" },
-    { img: "/assets/img/gallery/latest3.jpg.webp" },
-    { img: "/assets/img/gallery/latest4.jpg.webp" },
-  ];
+  testimonials = input<IFeaturedTestimonials[]>([]);
+  itemsPerSlide = 1;
   slideConfig = { "slidesToShow": 3, "slidesToScroll": 6 };
 
-  addSlide() {
-    this.slides.push({ img: "http://placehold.it/350x150/777777" })
-  }
-
-  removeSlide() {
-    this.slides.length = this.slides.length - 1;
-  }
-
-  slickInit(e: any) {
-    console.log('slick initialized');
-  }
-
-  breakpoint(e: any) {
-    console.log('breakpoint');
-  }
-
-  afterChange(e: any) {
-    console.log('afterChange');
-  }
-
-  beforeChange(e: any) {
-    console.log('beforeChange');
-  }
 }
